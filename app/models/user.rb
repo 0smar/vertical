469b82f26ae18a5c6eb7718551518fb1a185e4fd
0smar <osmar.roramas@gmail.com>
2016-08-09 13:00:41 -0500

    User model

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  validates :name, presence:true
+  validates :role, presence:true
+
+  ROLES = %i[superadmin instructor nutriologist student]
+end
